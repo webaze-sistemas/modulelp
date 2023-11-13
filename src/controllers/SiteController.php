cb363908de72ebe11f4bce0690589f5acc8089ea
Alexandre Vieira <avsilva@credipronto.com.br>
2023-11-13 06:30:16 -0300

    feat: add logic to save lead

--- src/controllers/SiteController.php
+++ src/controllers/SiteController.php
@@ -8,6 +8,42 @@ class SiteController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $model = new Lead();
+
+        $params = Yii::$app->request->queryParams;
+        Click::saveClick($params);
+
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            $model->load($post);
+            $model->save();
+
+            $params['lead_id'] = (string) $model->id;
+            Click::saveClick($params, true);
+
+            Yii::$app->session->setFlash('cadastro-ok', $model->name);
+            return $this->redirect(['site/ok']);
+        }
+
+        return $this->render('index', [
+            'model' => $model
+        ]);
+    }
+
+    public function actionOk()
+    {
+        $name = Yii::$app->session->getFlash('cadastro-ok');
+        if (!$name) {
+            return $this->redirect(['site/index']);
+        }
+
+        return $this->render('ok', [
+            'name' => $name
+        ]);
+    }
+
+    public function actionError()
+    {
+        return $this->render('error');
     }
 }
